859bae17e8abfa9cde830f9026fda80a51239857
chainx <chainx@hotmail.co.uk>
2021-06-29 21:18:13 +0100

    Update Write_invoices.py

--- Write_invoices.py
+++ Write_invoices.py
@@ -1,29 +1,34 @@
 from Dependencies import *
 
-def combine_payments(info):
-    payments=[]
+path = '/media/mike/E8/Orchard lodge accounting/'
+
+def combine_debts(info):
+    debts=[]
     temp=[info[0][0],[]]
     for n in range(len(info)):
         if info[n][0]==temp[0]: temp[1].append([info[n][1].replace('-',''),info[n][2],info[n][3]])
+        
         if info[n][0]!=temp[0] or n==len(info)-1:
             total=0
             for payment in temp[1]: total+=float(payment[0])
-            temp.append(str(total))
-            payments.append(temp)
+            temp.append("{:.2f}".format(total))
+            debts.append(temp)
+            
             temp=[info[n][0],[[info[n][1].replace('-',''),info[n][2],info[n][3]]]]
+            
             if n==len(info)-1: #Phillip wycherly only has one payment
                 temp.append(temp[1][0][0])
-                payments.append(temp)
-    return payments
+                debts.append(temp)
+    return debts
             
 
-def write_inovices(payments,date,year):
-    file = open('Invoices/Invoice number.txt', 'r') #Get invoice number
+def write_inovices(debts,date,year,file_number):
+    file = open(path+'Invoices/Invoice number.txt', 'r') #Get invoice number
     invoice_number = file.readline()
     file.close()
 
-    for resident in payments: #Payments should be a dictionary indexed by resident name
-        document = Document('Invoices/INVOICE TEMPLATE.DOCX')
+    for resident in debts: #debts should be a dictionary indexed by resident name
+        document = Document(path+'Invoices/INVOICE TEMPLATE.DOCX')
 
         for paragraph in document.paragraphs:
             if paragraph.text.count("INVOICE NUMB")==1: paragraph.text=paragraph.text.replace("INVOICE NUMB",invoice_number)
@@ -31,64 +36,69 @@ def write_inovices(payments,date,year):
             if paragraph.text.count("NAME OF RECIPIENT")==1: paragraph.text=paragraph.text.replace("NAME OF RECIPIENT",resident[0])
 
         count=0
-        for payment in resident[1]:            
+        for debt in resident[1]:            
             paragraph = document.tables[0].cell(0,0).paragraphs[7+count]
+
             reason=''
-            if payment[2]!='' and payment[2]!='MA': reason=' ('+payment[2]+')'
-            paragraph.text='From ' + payment[1] + reason
+            if debt[2]!='' and debt[2]!='MA': reason=' ('+debt[2]+')'
+            paragraph.text='From ' + debt[1] + reason
             
             paragraph =  document.tables[0].cell(0,1).paragraphs[8+count]
-            paragraph.text=chr(163)+"{:.2f}".format(float(payment[0])) #Write number with two decimal places
+            paragraph.text=chr(163)+str(debt[0])
             count+=1
 
         for paragraph in document.tables[0].cell(1,1).paragraphs:
-            if paragraph.text.count('TOTAL')==1: paragraph.text=paragraph.text.replace('TOTAL',"{:.2f}".format(float(resident[2])))
+            if paragraph.text.count('TOTAL')==1: paragraph.text=paragraph.text.replace('TOTAL',str(resident[2]))
 
-        document.save('Invoices/'+year+'/'+str(N)+'. '+date+'/'+invoice_number+' - '+resident[0]+'.docx')
+        document.save(path+'Invoices/'+year+'/'+str(file_number)+'. '+date+'/'+invoice_number+' - '+resident[0]+'.docx')
         invoice_number = "%05d" % (int(invoice_number)+1,)
         
-    file = open('Invoices/Invoice number.txt', 'w') #Update invoice number
+    file = open(path+'Invoices/Invoice number.txt', 'w') #Update invoice number
     file.write(invoice_number)
     file.close()
 
-#==============================================================================================================================================================================
-
-#Calculate today's date
-now = datetime.now()
-year = now.strftime("%Y")
-date= now.strftime("%d")+' '+now.strftime("%B")+' '+now.strftime("%Y")
-
 
 #==============================================================================================================================================================================
 
-#GOALS:
-#    -ADD FUNCTIONALITY FOR AUTOMATICALLY MOVING THE FILES FROM THE DOWNLOADS FOLDER AND RENAMING THEM
-#    -CREATE ERROR CORRECTION FOR MULTIPLE INVOICES PER RESIDENT (FOR INSTANCE P WYCHERLY IN 04/21)
+if __name__ == "__main__":
 
-N=len(glob.glob('Remittance advice/'+year+'/*.csv')) 
-statement=''
-for file in glob.glob('Remittance advice/'+year+'/*.csv'):
-    if int(file.split('/')[2].split('.')[0])==N: statement=file #file.split('/')[1].split('.')[0] is the file number
+    #Calculate today's date
+    now = datetime.now()
+    year = now.strftime("%Y")
+    date = now.strftime("%d %B %Y")
 
-#==============================================================================================================================================================================
+    if 'report_export.csv' in os.listdir('/home/mike/Downloads'):
+
+        (debts,debt_date) = read_sefton_file("/home/mike/Downloads/report_export.csv")
+
+        dates = [datetime.strptime(date,'%d/%m/%Y').strftime('%d %B') for date in debt_date.split(' to ')]
+        file_number = int(len(os.listdir("/media/mike/E8/Orchard lodge accounting/Remittance advice/"+year))/2) + 1
+        filename = str(file_number) + '. ' + dates[0] + ' - ' + dates[1]
+        
+
+    debts = combine_debts(debts) #Combines debts for the same resident and calculates the total
+
+    #Private residents
+    debts.append( ['Mrs Barbara Kelleher', [['2200', debt_date, '']], '2200'] )
+    debts.append( ['Mrs Everlyn Bailey', [['2160', debt_date, '']], '2160'] )
+    debts.append( ['Mrs Eleanor Cronin', [['2200', debt_date, '']], '2200'] )
+    debts.append( ['Mrs Veronica Curley', [['2200', debt_date, '']], '2200'] )
+    debts.append( ['Mr Ronald Taylor', [['2200', debt_date, '']], '2200'] )
 
 
-info=read_sefton_file(statement) #Read Sefton's csv file        
-payments=combine_payments(info) #Collect payments for each resident for invoicing
-
-payment_date = payments[len(payments)-1][1][0][1] #P. W is last and never has anomalies so is useful for getting the date
-
-#Private residents
-
+    #Printing invoices for manual error check
+    for resident in debts:
+        print(resident[0])
+        for payment in resident[1]: print(payment)
+        print('Total for client is '+resident[2]+'\n')
 
 
-for resident in payments:
-    print(resident[0])
-    for payment in resident[1]: print(payment)
-    print('Total for client is '+resident[2]+'\n')
-
-safety_check=input('Do you want to continue? (y/n)  ')
-if safety_check=='y':
-    os.makedirs('Invoices/'+year+'/'+str(N)+'. '+date) #Create new folder
-    write_inovices(payments,date,year) #Write invoices
-else: print('\n'*3+'Invoice writing aborted!')
+    #Checking before proceding to write invoices
+    safety_check=input('Do you want to continue? (y/n)  ')
+    if safety_check=='y':
+        os.makedirs(path+'Invoices/'+year+'/'+str(file_number)+'. '+date) #Create new folder
+        write_inovices(debts,date,year,file_number)                       #Write invoices
+        shutil.move("/home/mike/Downloads/report_export.csv", path+'Remittance advice/'+year+'/'+filename+'.csv')
+        shutil.move("/home/mike/Downloads/ActiveReports.PDF", path+'Remittance advice/'+year+'/'+filename+'.PDF')
+        
+    else: print('\n'*3+'Invoice writing aborted!')
