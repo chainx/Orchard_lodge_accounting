1a09453e170306d777eb8781e6e1683f949270a3
chainx <chainx@hotmail.co.uk>
2021-07-22 13:43:42 +0100

    Update Manage_files.py

--- Manage_files.py
+++ Manage_files.py
@@ -1,24 +1,25 @@
 from Dependencies import *
 
+path = '/media/mike/E8/Orchard lodge accounting/'
+
 def correct_invoice_number(invoice_number):
-    file = open('Invoices/Invoice number.txt', 'w') #Update invoice number
+    file = open(path+'Invoices/Invoice number.txt', 'w') #Update invoice number
     file.write(invoice_number) #For example '00843'
     file.close()
 
-def copy_debts(year_range):
-    for res in residents: #Create folder for resident if there isn't already one
-        if res.name.lower() not in [folder.lower() for folder in os.listdir('Invoices/Invoices sorted by resident')]:
-            os.makedirs('Invoices/Invoices sorted by resident/'+res.name)
+def organise_debts(year_range): #Sort the invoices into folders indexed by resident name
+    for res in residents: 
+        if res.name.lower() not in [folder.lower() for folder in os.listdir(path+'Invoices/Invoices sorted by resident')]:
+            os.makedirs(path+'Invoices/Invoices sorted by resident/'+res.name) #Create folder for resident if there isn't already one
 
     print('Copying starting')
     for year in year_range:
-        for folder_name in os.listdir('Invoices/'+year):
-            for filename in os.listdir('Invoices/'+year+'/'+folder_name):
+        for folder_name in os.listdir(path+'Invoices/'+year):
+            for filename in os.listdir(path+'Invoices/'+year+'/'+folder_name):
                 matched = False
                 for res in residents:                   
                     if res.name in filename:
-                        copyfile('Invoices/'+year+'/'+folder_name+'/'+filename, 'Invoices/Invoices sorted by resident/'+res.name+'/'+filename )
+                        copyfile(path+'Invoices/'+year+'/'+folder_name+'/'+filename, path+'Invoices/Invoices sorted by resident/'+res.name+'/'+filename )
                         matched = True
                 if matched==False: print(filename,'was not matched with any residents')
     print('Copying finished')   
-
